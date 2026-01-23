5784e960fc7371fe4cd7826908c35596a9ff67b6
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2026-01-23 13:06:22 +0500

    Added current year output into the footer

--- src/app/nz-footer/nz-footer.component.ts
+++ src/app/nz-footer/nz-footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nz-footer',
@@ -7,6 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './nz-footer.component.html',
   styleUrl: './nz-footer.component.css'
 })
-export class NzFooterComponent {
+export class NzFooterComponent implements OnInit {
+  currentYear: number = 0;
+  constructor() {}
 
+  ngOnInit(): void {
+    this.currentYear = new Date().getFullYear();
+  }
 }
